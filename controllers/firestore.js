2722131d29139273cc11f332bf736f7c078bd178
arash <kariznoviarash@gmail.com>
2024-01-27 18:08:07 -0400

    error-handler

--- controllers/firestore.js
+++ controllers/firestore.js
@@ -2,6 +2,7 @@ const admin = require('firebase-admin')
 const { getFirestore } = require('firebase-admin/firestore')
 const uuid = require('uuid').v4
 const serviceAccountKey = require('../firestore-creds.json')
+const HttpError = require('../models/http-error')
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccountKey),
@@ -23,13 +24,18 @@ const createText = async (req, res, next) => {
 
 // GET records API
 const readText = async (req, res, next) => {
-  const texts = db.collection('records')
-  const docs = await texts.get()
-
-  const responses = []
-  docs.forEach((doc) => {
-    responses.push(doc.data())
-  })
+  try {
+    throw new Error()
+    const texts = db.collection('records')
+    const docs = await texts.get()
+    const responses = []
+    docs.forEach((doc) => {
+      responses.push(doc.data())
+    })
+  } catch (err) {
+    const error = new HttpError('Unable to retreive records', 400)
+    return next(error)
+  }
 
   res.status(200).json({ responses })
 }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const fileRoutes = require('./routes/file-routes')
+const HttpError = require('./models/http-error')
 
 const PORT = process.env.PORT || 5000
 const app = express()
@@ -8,6 +9,19 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api', fileRoutes)
+
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route.', 404)
+  throw error
+})
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error)
+  }
+  res.status(error.code || 500)
+  res.json({ message: error.message || 'An unknown error occurred!' })
+})
 
 app.listen(PORT, () => {
   console.log('listening to port', PORT)
