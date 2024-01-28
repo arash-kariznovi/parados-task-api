3226d926c9e14a7a48f88b6efa3da9b8ffa026af
arash <kariznoviarash@gmail.com>
2024-01-27 23:04:12 -0400

    error-handler

--- controllers/firestore.js
+++ controllers/firestore.js
@@ -3,38 +3,63 @@ const { getFirestore } = require('firebase-admin/firestore')
 const uuid = require('uuid').v4
 const serviceAccountKey = require('../firestore-creds.json')
 const HttpError = require('../models/http-error')
+const { response } = require('express')
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccountKey),
-})
-const db = getFirestore()
+try {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccountKey),
+  })
+} catch (err) {
+  const error = new HttpError(
+    err.message ? err.message : 'Sorry! Please try again later',
+    500
+  )
+  return next(error)
+}
 
 // POST a text API
 const createText = async (req, res, next) => {
   const { text } = req.body
 
-  const response = await db.collection('records').doc(uuid()).set({
-    text: text,
-    id: uuid(),
-    createdAt: new Date(),
-  })
+  try {
+    const db = getFirestore()
+    const response = await db.collection('records').doc(uuid()).set({
+      text: text,
+      id: uuid(),
+      createdAt: new Date(),
+    })
+    console.log(response)
+  } catch (err) {
+    const error = new HttpError(
+      err.message ? err.message : 'Sorry! Please try again later',
+      500
+    )
+    return next(error)
+  }
 
-  res.send({ result: 'successful', response })
+  res.json({ result: 'The text record is stored successfully.' })
 }
 
 // GET records API
 const readText = async (req, res, next) => {
-  // try {
-  const texts = db.collection('records')
-  const docs = await texts.get()
   const responses = []
-  docs.forEach((doc) => {
-    responses.push(doc.data())
-  })
-  // } catch (err) {
-  //   const error = new HttpError('Unable to retreive records', 400)
-  //   return next(error)
-  // }
+  try {
+    const db = getFirestore()
+    const texts = db.collection('records')
+    const docs = await texts.get()
+    docs.forEach((doc) => {
+      responses.push(doc.data())
+    })
+    console.log(responses)
+  } catch (err) {
+    const error = new HttpError(
+      err.message
+        ? err.message
+        : 'Sorry! Unable to retreive the records at the moment',
+      500
+    )
+    return next(error)
+  }
 
   res.status(200).json({ responses })
 }
