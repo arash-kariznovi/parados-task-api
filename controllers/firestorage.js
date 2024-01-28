7ba0e15e422b1ee4bb2a9c3c679f0fdec1313849
arash <kariznoviarash@gmail.com>
2024-01-27 23:14:28 -0400

    error-handler-upload

--- controllers/firestorage.js
+++ controllers/firestorage.js
@@ -8,36 +8,66 @@ const {
 const firebase = require('firebase/app')
 const firebaseConfig = require('../firestorage-creds.json')
 
-// web app's Firebase configuration
-firebase.initializeApp(firebaseConfig)
-// Create a root reference
-const storage = getStorage()
+const HttpError = require('../models/http-error')
+
+try {
+  // web app's Firebase configuration
+  firebase.initializeApp(firebaseConfig)
+} catch (error) {
+  const err = new HttpError(
+    error.message ? error.message : 'Please try again later',
+    500
+  )
+}
 
 // POST file API
 const uploadFile = async (req, res, next) => {
+  // Create a root reference
+  const storage = getStorage()
   // Create a reference
   const storageRef = ref(storage, `files/${req.file.originalname}`)
 
-  uploadBytes(storageRef, req.file.buffer).then((ss) => {
-    console.log('file is uploaded.')
-  })
+  try {
+    uploadBytes(storageRef, req.file.buffer).then((ss) => {
+      console.log('file is uploaded.')
+    })
+  } catch (err) {
+    const error = new HttpError(
+      err.message
+        ? err.message
+        : 'Sorry! Unable to upload the files at the moment',
+      500
+    )
+    return next(error)
+  }
 
   return res.json({ res: 'API for uploading files' })
 }
 
 // GET files API
 const showFiles = async (req, res, next) => {
+  // Create a root reference
+  const storage = getStorage()
   // Find all the prefixes and items.
   const listRef = ref(storage, `files/`)
+  let fileData = []
+  try {
+    const result = await listAll(listRef)
+    const all = result.items.map(async (item) => {
+      const url = await getDownloadURL(ref(storage, `files/${item.name}`))
+      return { name: item.name, url }
+    })
 
-  const result = await listAll(listRef)
-
-  const all = result.items.map(async (item) => {
-    const url = await getDownloadURL(ref(storage, `files/${item.name}`))
-    return { name: item.name, url }
-  })
-
-  const fileData = await Promise.all(all)
+    fileData = await Promise.all(all)
+  } catch (err) {
+    const error = new HttpError(
+      err.message
+        ? err.message
+        : 'Sorry! Unable to retreive the files at the moment',
+      500
+    )
+    return next(error)
+  }
 
   return res.json({ res: fileData })
 }
